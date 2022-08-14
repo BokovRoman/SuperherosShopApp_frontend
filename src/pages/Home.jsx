5525366c7755a5ee63394903c87365cb45365e1e
BokovRoman <r.bokov1991@gmail.com>
2022-08-15 02:35:44 +0300

    prepare to parcel deploy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Grid from '@mui/material/Grid';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+import usePagination from './usePagination';
+
 
 import { Hero } from '../components/Hero';
 import { fetchHeroes } from '../redux/slices/heroes';
 
 
 export const Home = () => {
+  
   const dispatch = useDispatch();
     const userData= useSelector(state => state.auth.data);
     const {heroes } = useSelector(state => state.heroes);
@@ -18,13 +23,24 @@ export const Home = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    console.log(heroes);
+
+
+  let [page, setPage] = useState(1);
+  const PER_PAGE = 5;
+
+  const count = Math.ceil(heroes.items.length / PER_PAGE);
+  const _DATA = usePagination(heroes.items, PER_PAGE);
+
+  const handleChange = (e, p) => {
+      setPage(p);
+      _DATA.jump(p);
+  };
 
   return (
     <>
       <Grid container spacing={4}>
         <Grid xs={8} item>
-                  {(isHeroesLoading?[...Array(5)] : heroes.items).map((obj, index) =>
+                  {(isHeroesLoading?[...Array(5)] : _DATA.currentData()).map((obj, index) =>
                       isHeroesLoading ? (
                           <Hero key={index} isLoading={true}/>
                       ):
@@ -43,6 +59,16 @@ export const Home = () => {
         </Grid>
 
       </Grid>
+
+      <Stack spacing={2}>
+        <Pagination
+          count={count}
+          color="primary"
+          page={page}
+          onChange={handleChange}
+        />
+      </Stack>
+
     </>
   );
 };
